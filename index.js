17e4735d2e868629ec61a0181e59e485218d5051
K Vivek Kumar <134326034+v-house@users.noreply.github.com>
2023-09-15 22:58:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
