91a9f56a2973c3879077fa872d1c11adb129cc78
K Vivek Kumar <134326034+v-house@users.noreply.github.com>
2023-09-15 22:48:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const messages = [];
+
+app.get("/getMessages", (req, res) => {
+  const recentMessage = messages[messages.length - 1];
+  if (recentMessage) {
+    res.json([recentMessage]);
+  } else {
+    res.json([]);
+  }
+});
+
+app.post("/sendMessage", (req, res) => {
+  const { text, sender } = req.body;
+  if (text && sender) {
+    messages.push({ text, sender });
+    res.json({ success: true, message: "Message sent successfully" });
+  } else {
+    res.status(400).json({ success: false, message: "Invalid message format" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
